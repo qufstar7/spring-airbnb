30f897e8ab43ab3ac79554947bee89b41b5c9a35
junha <57350342+qufstar7@users.noreply.github.com>
2022-08-10 16:30:54 +0900

    유실 객체 생성

---
+++ src/main/java/kr/co/airbnb/vo/AccPrivacyType.java
@@ -0,0 +1,17 @@
+package kr.co.airbnb.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Alias("accPrivacyType")
+public class AccPrivacyType {
+
+}
