b4a61275d2dcca95f004481c14574b4720ccdfa8
dowoo <kimdow6@gmail.com>
2022-08-12 09:31:27 +0900

    예약 폼 수정

--- src/main/java/kr/co/airbnb/controller/ReservationController.java
+++ src/main/java/kr/co/airbnb/controller/ReservationController.java
@@ -1,5 +1,8 @@
 package kr.co.airbnb.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +10,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import kr.co.airbnb.annotation.LoginUser;
 import kr.co.airbnb.form.CardRegisterForm;
+import kr.co.airbnb.form.ReservationForm;
 import kr.co.airbnb.service.AccommodationService;
 import kr.co.airbnb.service.ReservationService;
 import kr.co.airbnb.vo.Accommodation;
 import kr.co.airbnb.vo.Reservation;
+import kr.co.airbnb.vo.User;
 
 @Controller
 @RequestMapping("/book")
+@SessionAttributes({"reservationForm"})
 public class ReservationController {
 	
 	@Autowired
@@ -42,11 +52,20 @@ public class ReservationController {
 		
 		reservationService.addNewCard(cardRegisterForm);
 				
+		return "/reservation/completed";
+	}
+	
+	@PostMapping
+	public String insert(@LoginUser User loginUser, @ModelAttribute("reservationForm") ReservationForm reservationForm,
+			SessionStatus sessionStatus) throws IOException {
 		return "redirect:/reservation/completed";
 	}
 
 	@GetMapping(path = "/completed")
-	public String completed() {
+	public String completed(Model model, int no) {
+		List<Reservation> reservations = reservationService.getAllReservationByUsers(no);
+		model.addAttribute("reservations",reservations);
+		
 		return"/reservation/completed";
 	}
 }
