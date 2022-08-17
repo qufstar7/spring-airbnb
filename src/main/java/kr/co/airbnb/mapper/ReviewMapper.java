65dcbd2d91d674d6ef3370196d68be380aef89da
minji <mjkim856@gmail.com>
2022-08-18 08:39:37 +0900

    user profile page에서 받은 리뷰/적은 리뷰 불러오기 구현 중

--- src/main/java/kr/co/airbnb/mapper/ReviewMapper.java
+++ src/main/java/kr/co/airbnb/mapper/ReviewMapper.java
@@ -1,6 +1,7 @@
 package kr.co.airbnb.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -20,5 +21,7 @@ public interface ReviewMapper {
 	Integer getOverdueReviewByNo(@Param("reservationNo") int reservationNo, @Param("userNo") int userNo);
 	
 	List<Review> getReviews(@Param("accNo") int accNo, int beginIndex, int endIndex, @Param("keyword") String keyword);
-	List<Review> getReviewsUnder(int accNo);
+	List<Review> getReceivedReviewByGuest(int no);	// 유저 정보 하단 리뷰 모음
+	List<Review> getReceivedReviewByHost(int no);	// 유저 정보 하단 리뷰 모음
+	
 }
